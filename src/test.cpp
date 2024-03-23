aec2e23a1206e7d2bb8fa8858de65f505824f1c7
Joshua Dahl <joshuadahl@nevada.unr.edu>
2024-03-23 12:52:06 -0700

    Unicode Identifiers
    Add support for identifying unicode identifiers the same way C++ does.

--- src/test.cpp
+++ src/test.cpp
@@ -69,7 +69,7 @@ struct ParseData: public ECS::ParseData<ECS::SkiplistAttributeStorage> {
 		return {res, location};
 	}
 	State lookahead(/*lex::detail::instantiation_of_lexer<lex::basic_lexer>*/ auto& lexer) { return lookahead(lexer, this->state); }
-	
+
 	State& lex(/*lex::detail::instantiation_of_lexer<lex::basic_lexer>*/ auto& lexer, const State& state) {
 		return this->state = lookahead(lexer, state);
 	}
@@ -94,6 +94,77 @@ struct ParseData: public ECS::ParseData<ECS::SkiplistAttributeStorage> {
 	}
 };
 
+#include <codecvt>
+#include <locale>
+#include "../thirdparty/unicode_ident.h"
+struct XIDIdentifierHead {
+	static std::optional<std::uint32_t> utf8_to_utf32(std::string_view utf8) {
+		uint32_t ch = 0;
+		size_t bytes = 0;
+
+		if ((utf8[0] & 0x80) == 0x00) {
+			// Single byte (ASCII)
+			ch = utf8[0];
+			bytes = 1;
+		} else if ((utf8[0] & 0xE0) == 0xC0) {
+			// Two bytes
+			if(utf8.size() < 2) return {};
+			ch = (utf8[0] & 0x1F) << 6;
+			ch |= (utf8[1] & 0x3F);
+			bytes = 2;
+		} else if ((utf8[0] & 0xF0) == 0xE0) {
+			// Three bytes
+			if(utf8.size() < 3) return {};
+			ch = (utf8[0] & 0x0F) << 12;
+			ch |= (utf8[1] & 0x3F) << 6;
+			ch |= (utf8[2] & 0x3F);
+			bytes = 3;
+		} else if ((utf8[0] & 0xF8) == 0xF0) {
+			// Four bytes
+			if(utf8.size() < 4) return {};
+			ch = (utf8[0] & 0x07) << 18;
+			ch |= (utf8[1] & 0x3F) << 12;
+			ch |= (utf8[2] & 0x3F) << 6;
+			ch |= (utf8[3] & 0x3F);
+			bytes = 4;
+		} else return {};
+		return ch;
+	}
+
+	constexpr static auto skip_if_invalid = true;
+	thread_local static std::string tmp;
+	thread_local static bool first /*= true*/;
+	static bool next_valid(size_t index, char next) {
+		if(index == 0) {
+			tmp.clear();
+			first = true;
+		}
+		tmp += next;
+		std::string debug = tmp;
+
+		auto res = utf8_to_utf32(tmp);
+		if(!res) return true;
+		tmp.clear(); // If nothing went wrong in the conversion then we need to move onto the next character
+		if(std::isspace(*res)) return false;
+		if(first) {
+			first = false;
+			return is_xid_start(*res);
+		}
+		return is_xid_continue(*res);
+	}
+	static bool token_valid(std::string_view token) {
+		// Make sure that the last character is valid!
+		bool valid = (token.size() >= 4 && utf8_to_utf32(token.substr(token.size() - 4)).has_value())
+			|| (token.size() >= 3 && utf8_to_utf32(token.substr(token.size() - 3)).has_value())
+			|| (token.size() >= 2 && utf8_to_utf32(token.substr(token.size() - 2)).has_value())
+			|| (token.size() >= 1 && utf8_to_utf32(token.substr(token.size() - 1)).has_value());
+		if(!token.empty() && std::isspace(token[0])) return false;
+		return valid;
+	}
+};
+thread_local std::string XIDIdentifierHead::tmp;
+thread_local bool XIDIdentifierHead::first = true;
+
 enum Tokens {
 	// Whitespace,
 	Newline,
@@ -116,9 +187,10 @@ constexpr lex::lexer<
 	lex::heads::token<Tokens::Equals, lex::heads::exact_character<'='>>,
 	lex::heads::token<Tokens::Open, lex::heads::exact_character<'('>>,
 	lex::heads::token<Tokens::Close, lex::heads::exact_character<')'>>,
-	lex::heads::token<Tokens::Identifier, lex::heads::ctre_regex<"[_a-zA-z][_a-zA-z0-9]*">>,
+	// lex::heads::token<Tokens::Identifier, lex::heads::ctre_regex<"[_a-zA-z][_a-zA-z0-9]*">>,
 	lex::heads::token<Tokens::Literal, lex::heads::ctre_regex<R"_((\+|-)?(\d+(\.\d*)?|\.\d+)([eE](\+|-)?\d+)?)_">>,
-	lex::heads::skip<lex::heads::ctre_regex<"\\s+">> // Skip whitespace!
+	lex::heads::skip<lex::heads::ctre_regex<"\\s+">>, // Skip whitespace!
+	lex::heads::token<Tokens::Identifier, XIDIdentifierHead>
 > lexer;
 
 constexpr lex::lexer<
@@ -130,9 +202,10 @@ constexpr lex::lexer<
 	lex::heads::token<Tokens::Equals, lex::heads::exact_character<'='>>,
 	lex::heads::token<Tokens::Open, lex::heads::exact_character<'('>>,
 	lex::heads::token<Tokens::Close, lex::heads::exact_character<')'>>,
-	lex::heads::token<Tokens::Identifier, lex::heads::ctre_regex<"[_a-zA-z][_a-zA-z0-9]*">>,
+	// lex::heads::token<Tokens::Identifier, lex::heads::ctre_regex<"[_a-zA-z][_a-zA-z0-9]*">>,
 	lex::heads::token<Tokens::Literal, lex::heads::ctre_regex<R"_((\+|-)?(\d+(\.\d*)?|\.\d+)([eE](\+|-)?\d+)?)_">>,
-	lex::heads::skip<lex::heads::ctre_regex<"\\s">> // Skip whitespace (one at a time, excluding newlines)!
+	lex::heads::skip<lex::heads::ctre_regex<"\\s">>, // Skip whitespace (one at a time, excluding newlines)!
+	lex::heads::token<Tokens::Identifier, XIDIdentifierHead>
 > lexerNewline;
 
 
