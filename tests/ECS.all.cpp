128b449fe9389f8c50436baeb73e75b3b958eb99
Joshua Dahl <joshuadahl@unr.edu>
2024-08-13 18:26:20 -0700

    [ECS] Removal and Sorting

--- tests/ECS.all.cpp
+++ tests/ECS.all.cpp
@@ -13,7 +13,7 @@ TEST_SUITE("ECS") {
 		{ZoneScopedN("Lookup"); CHECK(doir::ecs::doir_ecs_component_id_from_name("float") == 0);}
 		{ZoneScopedN("Lookup::NonExist"); CHECK(doir::ecs::doir_ecs_component_id_from_name("alice") == 2);}
 		{ZoneScopedN("Lookup::NonExistNorCreate"); CHECK(doir::ecs::doir_ecs_component_id_from_name("bob", false) == -1);}
-		{ZoneScopedN("Name"); 
+		{ZoneScopedN("Name");
 			CHECK(std::string_view(doir::ecs::doir_ecs_component_id_name(0)) == "float");
 			CHECK(std::string_view(doir::ecs::doir_ecs_component_id_name(1)) == "int");
 			CHECK(std::string_view(doir::ecs::doir_ecs_component_id_name(2)) == "alice");
@@ -55,145 +55,229 @@ TEST_SUITE("ECS") {
 		FrameMark;
 	}
 
-	// TEST_CASE("doir::ecs::Removal") {
-	// 	ZoneScoped;
-	// 	doir::ecs::scene scene;
-	// 	doir::ecs::entity e = scene.create_entity();
-	// 	CHECK(e == 0);
-	// 	scene.add_component<float>(e);
-	// 	CHECK(scene.release_entity(e));
+	TEST_CASE("doir::ecs::Removal") {
+#ifdef DOIR_ENABLE_BENCHMARKING
+		ankerl::nanobench::Bench().run("doir::ecs::Removal", []{
+#endif
+			ZoneScopedN("doir::ecs::Removal");
+			doir::ecs::Module module;
+			doir::ecs::entity_t e = module.create_entity();
+			CHECK(e == 1);
+			module.add_component<float>(e);
+			CHECK(module.release_entity(e));
 
-	// 	e = scene.create_entity();
-	// 	CHECK(e == 0);
-	// 	CHECK(scene.has_component<float>(e) == false);
+			e = module.create_entity();
+			CHECK(e == 1);
+			CHECK(module.has_component<float>(e) == false);
 
-	// 	doir::ecs::entity e2 = scene.create_entity();
-	// 	CHECK(e2 == 1);
-	// 	doir::ecs::entity e3 = scene.create_entity();
-	// 	CHECK(e3 == 2);
-	// 	*scene.add_component<float>(e) = 1;
-	// 	*scene.add_component<float>(e2) = 2;
-	// 	*scene.add_component<float>(e3) = 3;
-	// 	CHECK(*scene.get_component<float>(e) == 1);
-	// 	CHECK(*scene.get_component<float>(e2) == 2);
-	// 	CHECK(*scene.get_component<float>(e3) == 3);
+			doir::ecs::entity_t e2 = module.create_entity();
+			CHECK(e2 == 2);
+			doir::ecs::entity_t e3 = module.create_entity();
+			CHECK(e3 == 3);
+			module.add_component<float>(e) = 1;
+			module.add_component<float>(e2) = 2;
+			module.add_component<float>(e3) = 3;
+			CHECK(module.get_component<float>(e) == 1);
+			CHECK(module.get_component<float>(e2) == 2);
+			CHECK(module.get_component<float>(e3) == 3);
 
-	// 	CHECK(scene.remove_component<float>(e2) == true);
-	// 	CHECK(*scene.get_component<float>(e) == 1);
-	// 	CHECK(scene.has_component<float>(e2) == false);
-	// 	CHECK(*scene.get_component<float>(e3) == 3);
-	// }
+			CHECK(module.remove_component<float>(e2) == true);
+			CHECK(module.get_component<float>(e) == 1);
+			CHECK(module.has_component<float>(e2) == false);
+			CHECK(module.get_component<float>(e3) == 3);
+			// module.should_leak = true; // Don't bother cleaning up after ourselves...
+#ifdef DOIR_ENABLE_BENCHMARKING
+		});
+#endif
+		FrameMark;
+	}
+
+	TEST_CASE("doir::ecs::SortByValue") {
+#ifdef DOIR_ENABLE_BENCHMARKING
+		ankerl::nanobench::Bench().run("doir::ecs::SortByValue", []{
+#endif
+			ZoneScopedN("doir::ecs::SortByValue");
+			doir::ecs::Module module;
+			auto e0 = module.create_entity();
+			module.add_component<float>(e0) = 3;
+			auto e1 = module.create_entity();
+			module.add_component<float>(e1) = 27;
+			auto e2 = module.create_entity();
+			module.add_component<float>(e2) = 5;
+			auto e3 = module.create_entity();
+			module.add_component<float>(e3) = 0;
+
+			auto& storage = module.get_storage<float>();
+			storage.sort_by_value<float>(module);
+			float* storage_as_float = storage.data<float>();
+			CHECK(storage_as_float[0] == 0);
+			CHECK(module.get_component<float>(e0) == 3);
+			CHECK(storage_as_float[1] == 3);
+			CHECK(module.get_component<float>(e1) == 27);
+			CHECK(storage_as_float[2] == 5);
+			CHECK(module.get_component<float>(e2) == 5);
+			CHECK(storage_as_float[3] == 27);
+			CHECK(module.get_component<float>(e3) == 0);
+			// module.should_leak = true; // Don't bother cleaning up after ourselves...
+#ifdef DOIR_ENABLE_BENCHMARKING
+		});
+#endif
+		FrameMark;
+	}
+
+	TEST_CASE("doir::ecs::SortMontonic") {
+#ifdef DOIR_ENABLE_BENCHMARKING
+		ankerl::nanobench::Bench().run("doir::ecs::SortMontonic", []{
+#endif
+			ZoneScopedN("doir::ecs::SortMontonic");
+			doir::ecs::Module module;
+			auto e0 = module.create_entity();
+			auto e1 = module.create_entity();
+			auto e2 = module.create_entity();
+			auto e3 = module.create_entity();
+			module.add_component<float>(e3) = 0;
+			module.add_component<float>(e0) = 3;
+			module.add_component<float>(e2) = 5;
+			module.add_component<float>(e1) = 27;
+
+			auto& storage = module.get_storage<float>();
+			storage.sort_monotonic<float>(module);
+			// module.make_monotonic();
+			float* storage_as_float = storage.data<float>();
+			CHECK(storage_as_float[0] == 3);
+			CHECK(module.get_component<float>(e0) == 3);
+			CHECK(storage_as_float[1] == 27);
+			CHECK(module.get_component<float>(e1) == 27);
+			CHECK(storage_as_float[2] == 5);
+			CHECK(module.get_component<float>(e2) == 5);
+			CHECK(storage_as_float[3] == 0);
+			CHECK(module.get_component<float>(e3) == 0);
+			// module.should_leak = true; // Don't bother cleaning up after ourselves...
+#ifdef DOIR_ENABLE_BENCHMARKING
+		});
+#endif
+		FrameMark;
+	}
+
+	TEST_CASE("doir::ecs::WithEntity") {
+#ifdef DOIR_ENABLE_BENCHMARKING
+		ankerl::nanobench::Bench().run("doir::ecs::WithEntity", []{
+#endif
+			ZoneScopedN("doir::ecs::WithEntity");
+			doir::ecs::Module module;
+			doir::ecs::entity_t e = module.create_entity();
+			CHECK(e == 1);
+			CHECK((module.add_component<doir::ecs::with_entity<float>>(e).value = 5) == 5);
+			CHECK(module.get_component<doir::ecs::with_entity<float>>(e).entity == e);
+			CHECK(module.get_component<doir::ecs::with_entity<float>>(e) == 5);
+			CHECK(module.get_component<doir::ecs::with_entity<float>>(e).entity == e);
+			module.get_component<doir::ecs::with_entity<float>>(e).value = 6;
+			CHECK(module.get_component<doir::ecs::with_entity<float>>(e) == 6);
+			CHECK(module.get_component<doir::ecs::with_entity<float>>(e).entity == e);
+
+			doir::ecs::entity_t e2 = module.create_entity();
+			CHECK(e2 == 2);
+			CHECK((module.add_component<doir::ecs::with_entity<float>>(e2).value = 5) == 5);
+			CHECK(module.get_component<doir::ecs::with_entity<float>>(e2).entity == e2);
+			CHECK(module.get_component<doir::ecs::with_entity<float>>(e2) == 5);
+			CHECK(module.get_component<doir::ecs::with_entity<float>>(e) == 6);
+			CHECK(module.get_component<doir::ecs::with_entity<float>>(e2).entity == e2);
+
+			module.get_storage<doir::ecs::with_entity<float>>().sort_by_value<doir::ecs::with_entity<float>>(module);
+			CHECK(module.get_component<doir::ecs::with_entity<float>>(e) == 6);
+			CHECK(module.get_component<doir::ecs::with_entity<float>>(e).entity == e);
+			CHECK(module.get_component<doir::ecs::with_entity<float>>(e2) == 5);
+			CHECK(module.get_component<doir::ecs::with_entity<float>>(e2).entity == e2);
+
+			module.get_storage<doir::ecs::with_entity<float>>().sort_monotonic<doir::ecs::with_entity<float>>(module);
+			CHECK(module.get_component<doir::ecs::with_entity<float>>(e) == 6);
+			CHECK(module.get_component<doir::ecs::with_entity<float>>(e).entity == e);
+			CHECK(module.get_component<doir::ecs::with_entity<float>>(e2) == 5);
+			CHECK(module.get_component<doir::ecs::with_entity<float>>(e2).entity == e2);
+			// module.should_leak = true; // Don't bother cleaning up after ourselves...
+#ifdef DOIR_ENABLE_BENCHMARKING
+		});
+#endif
+		FrameMark;
+	}
+
+	TEST_CASE("doir::ecs::UniqueTag") {
+#ifdef DOIR_ENABLE_BENCHMARKING
+		ankerl::nanobench::Bench().run("doir::ecs::UniqueTag", []{
+#endif
+			ZoneScopedN("doir::ecs::UniqueTag");
+			doir::ecs::Module module;
+			auto e0 = module.create_entity();
+			auto e1 = module.create_entity();
+			auto e2 = module.create_entity();
+			auto e3 = module.create_entity();
+			module.add_component<float>(e3) = 0;
+			module.add_component<float>(e0) = 3;
+			module.add_component<float>(e2) = 5;
+			module.add_component<float>(e1) = 27;
+			module.add_component<float, 1>(e3) = 27;
+			module.add_component<float, 1>(e0) = 5;
+			module.add_component<float, 1>(e2) = 3;
+			module.add_component<float, 1>(e1) = 0;
+
+			{
+				auto& storage = module.get_storage<float>();
+				storage.sort_monotonic<float>(module);
+				float* storage_as_float = storage.data<float>();
+				CHECK(storage_as_float[0] == 3);
+				CHECK(module.get_component<float>(e0) == 3);
+				CHECK(storage_as_float[1] == 27);
+				CHECK(module.get_component<float>(e1) == 27);
+				CHECK(storage_as_float[2] == 5);
+				CHECK(module.get_component<float>(e2) == 5);
+				CHECK(storage_as_float[3] == 0);
+				CHECK(module.get_component<float>(e3) == 0);
+			}
+			{
+				auto& storage = module.get_storage<float, 1>();
+				storage.sort_monotonic<float, 1>(module);
+				float* storage_as_float = storage.data<float>();
+				CHECK(storage_as_float[0] == 5);
+				CHECK(module.get_component<float, 1>(e0) == 5);
+				CHECK(storage_as_float[1] == 0);
+				CHECK(module.get_component<float, 1>(e1) == 0);
+				CHECK(storage_as_float[2] == 3);
+				CHECK(module.get_component<float, 1>(e2) == 3);
+				CHECK(storage_as_float[3] == 27);
+				CHECK(module.get_component<float, 1>(e3) == 27);
+			}
+			// module.should_leak = true; // Don't bother cleaning up after ourselves...
+#ifdef DOIR_ENABLE_BENCHMARKING
+		});
+#endif
+		FrameMark;
+	}
 
 	// TEST_CASE("doir::ecs::Query") {
 	// 	ZoneScoped;
-	// 	doir::ecs::scene scene;
-	// 	*scene.add_component<float>(scene.create_entity()) = 1;
-	// 	*scene.add_component<float>(scene.create_entity()) = 2;
-	// 	*scene.add_component<float>(scene.create_entity()) = 3;
+	// 	doir::ecs::module module;
+	// 	*module.add_component<float>(module.create_entity()) = 1;
+	// 	*module.add_component<float>(module.create_entity()) = 2;
+	// 	*module.add_component<float>(module.create_entity()) = 3;
 
-	// 	for(auto [e, value]: doir::ecs::query<doir::ecs::include_entity, float>(scene))
+	// 	for(auto [e, value]: doir::ecs::query<doir::ecs::include_entity, float>(module))
 	// 		CHECK(e + 1 == value);
 
-	// 	for(auto [e, value]: doir::ecs::query<doir::ecs::include_entity, std::optional<float>>(scene))
+	// 	for(auto [e, value]: doir::ecs::query<doir::ecs::include_entity, std::optional<float>>(module))
 	// 		CHECK(e + 1 == *value);
 
-	// 	for(auto [e, value]: doir::ecs::query<doir::ecs::include_entity, doir::ecs::or_<float, double>>(scene)) {
+	// 	for(auto [e, value]: doir::ecs::query<doir::ecs::include_entity, doir::ecs::or_<float, double>>(module)) {
 	// 		CHECK(e + 1 == std::get<std::reference_wrapper<float>>(value));
 	// 		REQUIRE_THROWS((void)std::get<std::reference_wrapper<double>>(value));
 	// 	}
 	// }
 
-	// TEST_CASE("doir::ecs::SortByValue") {
-	// 	ZoneScoped;
-	// 	doir::ecs::scene scene;
-	// 	auto e0 = scene.create_entity();
-	// 	*scene.add_component<float>(e0) = 3;
-	// 	auto e1 = scene.create_entity();
-	// 	*scene.add_component<float>(e1) = 27;
-	// 	auto e2 = scene.create_entity();
-	// 	*scene.add_component<float>(e2) = 5;
-	// 	auto e3 = scene.create_entity();
-	// 	*scene.add_component<float>(e3) = 0;
-
-	// 	auto& storage = *scene.get_storage<float>();
-	// 	storage.sort_by_value<float>(scene);
-	// 	float* storage_as_float = (float*)storage.data.data();
-	// 	CHECK(storage_as_float[0] == 0);
-	// 	CHECK(*scene.get_component<float>(e0) == 3);
-	// 	CHECK(storage_as_float[1] == 3);
-	// 	CHECK(*scene.get_component<float>(e1) == 27);
-	// 	CHECK(storage_as_float[2] == 5);
-	// 	CHECK(*scene.get_component<float>(e2) == 5);
-	// 	CHECK(storage_as_float[3] == 27);
-	// 	CHECK(*scene.get_component<float>(e3) == 0);
-	// }
-
-	// TEST_CASE("doir::ecs::SortMontonic") {
-	// 	ZoneScoped;
-	// 	doir::ecs::scene scene;
-	// 	auto e0 = scene.create_entity();
-	// 	auto e1 = scene.create_entity();
-	// 	auto e2 = scene.create_entity();
-	// 	auto e3 = scene.create_entity();
-	// 	*scene.add_component<float>(e3) = 0;
-	// 	*scene.add_component<float>(e0) = 3;
-	// 	*scene.add_component<float>(e2) = 5;
-	// 	*scene.add_component<float>(e1) = 27;
-
-	// 	auto& storage = *scene.get_storage<float>();
-	// 	storage.sort_monotonic<float>(scene);
-	// 	// scene.make_monotonic();
-	// 	float* storage_as_float = (float*)storage.data.data();
-	// 	CHECK(storage_as_float[0] == 3);
-	// 	CHECK(*scene.get_component<float>(e0) == 3);
-	// 	CHECK(storage_as_float[1] == 27);
-	// 	CHECK(*scene.get_component<float>(e1) == 27);
-	// 	CHECK(storage_as_float[2] == 5);
-	// 	CHECK(*scene.get_component<float>(e2) == 5);
-	// 	CHECK(storage_as_float[3] == 0);
-	// 	CHECK(*scene.get_component<float>(e3) == 0);
-	// }
-
-	// TEST_CASE("doir::ecs::WithEntity") {
-	// 	ZoneScoped;
-	// 	doir::ecs::scene scene;
-	// 	doir::ecs::entity e = scene.create_entity();
-	// 	CHECK(e == 0);
-	// 	CHECK((scene.add_component<doir::ecs::with_entity<float>>(e)->value = 5) == 5);
-	// 	CHECK(scene.get_component<doir::ecs::with_entity<float>>(e)->entity == e);
-	// 	CHECK(*scene.get_component<doir::ecs::with_entity<float>>(e) == 5);
-	// 	CHECK(scene.get_component<doir::ecs::with_entity<float>>(e)->entity == e);
-	// 	scene.get_component<doir::ecs::with_entity<float>>(e)->value = 6;
-	// 	CHECK(*scene.get_component<doir::ecs::with_entity<float>>(e) == 6);
-	// 	CHECK(scene.get_component<doir::ecs::with_entity<float>>(e)->entity == e);
-
-	// 	doir::ecs::entity e2 = scene.create_entity();
-	// 	CHECK(e2 == 1);
-	// 	CHECK((scene.add_component<doir::ecs::with_entity<float>>(e2)->value = 5) == 5);
-	// 	CHECK(scene.get_component<doir::ecs::with_entity<float>>(e2)->entity == e2);
-	// 	CHECK(*scene.get_component<doir::ecs::with_entity<float>>(e2) == 5);
-	// 	CHECK(*scene.get_component<doir::ecs::with_entity<float>>(e) == 6);
-	// 	CHECK(scene.get_component<doir::ecs::with_entity<float>>(e2)->entity == e2);
-
-	// 	scene.get_storage<doir::ecs::with_entity<float>>()->sort_by_value<doir::ecs::with_entity<float>>(scene);
-	// 	CHECK(*scene.get_component<doir::ecs::with_entity<float>>(e) == 6);
-	// 	CHECK(scene.get_component<doir::ecs::with_entity<float>>(e)->entity == e);
-	// 	CHECK(*scene.get_component<doir::ecs::with_entity<float>>(e2) == 5);
-	// 	CHECK(scene.get_component<doir::ecs::with_entity<float>>(e2)->entity == e2);
-
-	// 	scene.get_storage<doir::ecs::with_entity<float>>()->sort_monotonic<doir::ecs::with_entity<float>>(scene);
-	// 	CHECK(*scene.get_component<doir::ecs::with_entity<float>>(e) == 6);
-	// 	CHECK(scene.get_component<doir::ecs::with_entity<float>>(e)->entity == e);
-	// 	CHECK(*scene.get_component<doir::ecs::with_entity<float>>(e2) == 5);
-	// 	CHECK(scene.get_component<doir::ecs::with_entity<float>>(e2)->entity == e2);
-	// }
-
 	// TEST_CASE("doir::ecs::typed") {
 	// 	ZoneScoped;
-	// 	doir::ecs::scene scene;
-	// 	doir::ecs::entity e = scene.create_entity();
-	// 	*scene.add_component<float>(e) = 5;
-	// 	auto& storage = *get_adapted_component_storage<doir::ecs::typed::component_storage<float>>(scene);
+	// 	doir::ecs::module module;
+	// 	doir::ecs::entity e = module.create_entity();
+	// 	*module.add_component<float>(e) = 5;
+	// 	auto& storage = *get_adapted_component_storage<doir::ecs::typed::component_storage<float>>(module);
 	// 	CHECK(*storage.get(e) == 5);
 	// }
 
@@ -201,65 +285,24 @@ TEST_SUITE("ECS") {
 	// 	ZoneScoped;
 	// 	using C = doir::ecs::hashtable::component_storage<int>::component_type;
 
-	// 	doir::ecs::scene scene;
-	// 	doir::ecs::entity first = scene.create_entity();
+	// 	doir::ecs::module module;
+	// 	doir::ecs::entity first = module.create_entity();
 	// 	doir::ecs::entity current = first;
 	// 	for(size_t i = 0; i < 100; ++i) {
-	// 		get_key_and_mark_occupied<int>(scene.add_component<C>(current)) = current;
-	// 		current = scene.create_entity();
+	// 		get_key_and_mark_occupied<int>(module.add_component<C>(current)) = current;
+	// 		current = module.create_entity();
 	// 	}
 
-	// 	auto& hashtable = *get_adapted_component_storage<doir::ecs::hashtable::component_storage<int>>(scene);
-	// 	CHECK(hashtable.rehash(scene) == true);
+	// 	auto& hashtable = *get_adapted_component_storage<doir::ecs::hashtable::component_storage<int>>(module);
+	// 	CHECK(hashtable.rehash(module) == true);
 	// 	for(doir::ecs::entity e = first; e < first + 100; ++e) {
 	// 		CHECK(*hashtable.find(e) == e);
-	// 		CHECK(get_key<int>(scene.get_component<C>(*hashtable.find(e))) == e);
+	// 		CHECK(get_key<int>(module.get_component<C>(*hashtable.find(e))) == e);
 	// 	}
 	// }
 
-	// TEST_CASE("doir::ecs::UniqueTag") {
-	// 	ZoneScoped;
-	// 	doir::ecs::scene scene;
-	// 	auto e0 = scene.create_entity();
-	// 	auto e1 = scene.create_entity();
-	// 	auto e2 = scene.create_entity();
-	// 	auto e3 = scene.create_entity();
-	// 	*scene.add_component<float>(e3) = 0;
-	// 	*scene.add_component<float>(e0) = 3;
-	// 	*scene.add_component<float>(e2) = 5;
-	// 	*scene.add_component<float>(e1) = 27;
-	// 	*scene.add_component<float, 1>(e3) = 27;
-	// 	*scene.add_component<float, 1>(e0) = 5;
-	// 	*scene.add_component<float, 1>(e2) = 3;
-	// 	*scene.add_component<float, 1>(e1) = 0;
-
-	// 	{
-	// 		auto& storage = *scene.get_storage<float>();
-	// 		storage.sort_monotonic<float>(scene);
-	// 		// scene.make_monotonic();
-	// 		float* storage_as_float = (float*)storage.data.data();
-	// 		CHECK(storage_as_float[0] == 3);
-	// 		CHECK(*scene.get_component<float>(e0) == 3);
-	// 		CHECK(storage_as_float[1] == 27);
-	// 		CHECK(*scene.get_component<float>(e1) == 27);
-	// 		CHECK(storage_as_float[2] == 5);
-	// 		CHECK(*scene.get_component<float>(e2) == 5);
-	// 		CHECK(storage_as_float[3] == 0);
-	// 		CHECK(*scene.get_component<float>(e3) == 0);
-	// 	}
-	// 	{
-	// 		auto& storage = *scene.get_storage<float, 1>();
-	// 		storage.sort_monotonic<float, 1>(scene);
-	// 		// scene.make_monotonic();
-	// 		float* storage_as_float = (float*)storage.data.data();
-	// 		CHECK(storage_as_float[0] == 5);
-	// 		CHECK(*scene.get_component<float, 1>(e0) == 5);
-	// 		CHECK(storage_as_float[1] == 0);
-	// 		CHECK(*scene.get_component<float, 1>(e1) == 0);
-	// 		CHECK(storage_as_float[2] == 3);
-	// 		CHECK(*scene.get_component<float, 1>(e2) == 3);
-	// 		CHECK(storage_as_float[3] == 27);
-	// 		CHECK(*scene.get_component<float, 1>(e3) == 27);
-	// 	}
-	// }
+	TEST_CASE("doir::ecs::component_id_free_maps") {
+		ZoneScopedN("doir::ecs::component_id_free_maps");
+		doir::ecs::component_id_free_maps();
+	}
 }
