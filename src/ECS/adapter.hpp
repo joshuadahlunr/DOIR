c408febbad8dc1f9692a5cc84fba3f33e588ca4b
Joshua Dahl <joshuadahl@unr.edu>
2024-09-10 15:52:49 -0700

    [ECS] Hashtable

---
+++ src/ECS/adapter.hpp
@@ -0,0 +1,429 @@
+#ifndef __ECS_ADAPTER_HPP__
+#define __ECS_ADAPTER_HPP__
+
+#include "ecs.hpp"
+#include "fp/dynarray.h"
+#include "fp/hash.h"
+
+#include <bit>
+#include <cmath>
+#include <cstdint>
+#include <span>
+#include <type_traits>
+
+namespace doir::ecs {
+	template<typename T>
+	T& get_adapted_storage(TrivialModule& module) {
+		auto& res = module.get_storage<typename T::component_type>();
+		return {*(T*)&res};
+	}
+
+	namespace typed {
+		template<typename Tcomponent, size_t Unique = 0>
+		struct Storage : public doir::ecs::Storage {
+			using Base = doir::ecs::Storage;
+			using component_type = Tcomponent;
+			using Base::Base;
+			inline const Tcomponent& get(entity_t e) const { return Base::get<Tcomponent>(e); }
+			inline Tcomponent& get(entity_t e) { return Base::get<Tcomponent>(e); }
+			void allocate(size_t count = 1) { Base::allocate<Tcomponent>(count); }
+			inline Tcomponent& get_or_allocate(entity_t e) { return Base::get_or_allocate<Tcomponent>(e); }
+			inline bool remove(TrivialModule& module, entity_t e) { return Base::remove<Tcomponent, Unique>(module, e); }
+			inline void reorder(TrivialModule& module, fp_view(size_t) order) { Base::reorder<Tcomponent, Unique>(module, order); }
+			template<typename F, bool with_entities = false>
+			inline void sort(TrivialModule& module, const F& comparator) { Base::sort<Tcomponent, F, with_entities, Unique>(module, comparator); }
+			inline void sort_by_value(TrivialModule& module) { Base::sort_by_value<Tcomponent, Unique>(module); }
+			inline void sort_monotonic(TrivialModule& module) { Base::sort_monotonic<Tcomponent, Unique>(module); }
+			inline bool swap(size_t a, std::optional<size_t> b = {}) { return Base::swap<Tcomponent>(a, b); }
+			inline bool swap(TrivialModule& module, size_t a, std::optional<size_t> b = {}, bool swap_if_one_elementless = false) {
+				return Base::swap<Tcomponent, Unique>(module, a, b, swap_if_one_elementless);
+			}
+
+			inline Tcomponent* data() { return Base::data<Tcomponent>(); }
+			inline const Tcomponent* data() const { return Base::data<Tcomponent>(); }
+			inline fp_view(Tcomponent) view() { return {data(), Base::size()}; }
+			inline fp_view(const Tcomponent) view() const { return {data(), Base::size()}; }
+		};
+	}
+
+	namespace hashtable {
+		template<typename T>
+		struct is_costly_to_compare : public std::false_type {};
+		template<>
+		struct is_costly_to_compare<std::string> : public std::true_type {};
+		template<>
+		struct is_costly_to_compare<std::string_view> : public std::true_type {};
+
+		template<typename T>
+		constexpr static bool is_costly_to_compare_v = is_costly_to_compare<T>::value;
+
+		namespace detail {
+			struct hash_entry_base {
+				uint32_t hopInfo = 0;  // Bitmask to track neighbors (occupied stored in highest bit)
+
+				bool is_occupied() const {
+					return hopInfo & (1 << 31);
+				}
+				void set_occupied(bool value) {
+					if(value) hopInfo |=(1 << 31);
+					else hopInfo &= ~(1 << 31);
+				}
+			};
+
+			template<typename Tkey, typename Tvalue>
+			struct hash_entry : public hash_entry_base {
+				Tkey key;
+				Tvalue value;
+
+				static inline void swap_entities(hash_entry& e, ecs::entity_t eA, ecs::entity_t eB)
+					requires(requires(Tkey t){Tkey::swap_entities(t, eA, eB);} || requires(Tvalue t){Tvalue::swap_entities(t, eA, eB);})
+				{
+					if constexpr(requires(Tkey t){Tkey::swap_entities(t, eA, eB);})
+						Tkey::swap_entities(e.key, eA, eB);
+					if constexpr(requires(Tvalue t){Tvalue::swap_entities(t, eA, eB);})
+						Tvalue::swap_entities(e.value, eA, eB);
+				}
+			};
+			template<typename Tkey>
+			struct hash_entry<Tkey, void> : public hash_entry_base {
+				Tkey key;
+
+				static inline void swap_entities(hash_entry& e, ecs::entity_t eA, ecs::entity_t eB)
+					requires(requires(Tkey t){Tkey::swap_entities(t, eA, eB);})
+				{
+					Tkey::swap_entities(e.key, eA, eB);
+				}
+			};
+
+			template<typename Tkey, typename Tvalue>
+			struct hash_entry_with_hash: public hash_entry<Tkey, Tvalue> {
+				size_t hash;
+
+				using Entry = hash_entry<Tkey, Tvalue>;
+				static inline void swap_entities(hash_entry_with_hash& e, ecs::entity_t eA, ecs::entity_t eB)
+					requires(requires(Entry e){Entry::swap_entities(e, eA, eB);})
+				{
+					Entry::swap_entities(e, eA, eB);
+				}
+			};
+		}
+
+		template<typename Tkey, typename Tvalue = void>
+		struct component_wrapper : public with_entity<std::conditional_t<
+			is_costly_to_compare_v<Tkey>, detail::hash_entry_with_hash<Tkey, Tvalue>, detail::hash_entry<Tkey, Tvalue>>
+		> {
+			using Entry = std::conditional_t<is_costly_to_compare_v<Tkey>, detail::hash_entry_with_hash<Tkey, Tvalue>, detail::hash_entry<Tkey, Tvalue>>;
+			using Base = with_entity<Entry>;
+			static inline void swap_entities(component_wrapper& w, ecs::entity_t eA, ecs::entity_t eB) {
+				Base::swap_entities(w, eA, eB);
+				if constexpr(requires(Entry e){Entry::swap_entities(e, eA, eB);})
+					Entry::swap_entities(w.value, eA, eB);
+			}
+		};
+
+		/*constexpr*/ size_t one_over_one_minus(float factor)
+#ifdef ECS_IMPLEMENTATION
+		{
+			assert(0 < factor); assert(factor < 1);
+			return std::round(1 / (1 - factor));
+		}
+#else
+		;
+#endif
+
+		// An adapter over a component storage which treats the underlying data as a hopscotch hash table
+		template<
+			typename Tkey,
+			typename Tvalue = void,
+			typename Hash = std::hash<Tkey>,
+			size_t Unique = 0,
+			size_t OneOverOneMinusMaxLoadFactor = /*one_over_one_minus(.95)*/20,
+			size_t NeighborhoodSize = 8,
+			size_t MaxRetries = 5
+		>
+		class Storage
+			: public typed::Storage<
+				component_wrapper<ecs::detail::remove_with_entity_t<Tkey>, ecs::detail::remove_with_entity_t<Tvalue>>, Unique
+			>
+		{
+		protected:
+			using key_type = ecs::detail::remove_with_entity_t<Tkey>;
+			using value_type = ecs::detail::remove_with_entity_t<Tvalue>;
+			using component_t = component_wrapper<key_type, value_type>;
+			using Base = typed::Storage<component_t, Unique>;
+
+			static constexpr float maxLoadFactor = 1 - 1.0 / OneOverOneMinusMaxLoadFactor;
+			static constexpr float store_hash = is_costly_to_compare_v<key_type>;
+
+			size_t current_size() const {
+				DOIR_ZONE_SCOPED_AGRO;
+				size_t size = 0;
+				for(auto& e: Base::span())
+					if(e.is_occupied())
+						++size;
+				return size;
+			}
+
+			// const key_type& key(const kv_pair& key_value) {
+			// 	if constexpr(has_value_type)
+			// 		return key_value.first;
+			// 	else return key_value;
+			// }
+
+			inline float load_factor() const {
+				DOIR_ZONE_SCOPED_AGRO;
+				return float(current_size()) / Base::size();
+			}
+
+			inline size_t hash(const key_type& key) const {
+				DOIR_ZONE_SCOPED_AGRO;
+				return Hash{}(key) % Base::size();
+			}
+
+			std::optional<size_t> find_position(const key_type& key) const {
+				DOIR_ZONE_SCOPED_AGRO;
+				size_t hash = this->hash(key);
+				auto data = Base::data();
+				auto size = Base::size();
+				for(size_t i = 0; i < NeighborhoodSize; ++i) {
+					size_t probe = (hash + i) % size;
+					if constexpr(store_hash) if(!data[probe]->is_occupied() || data[probe]->hash != hash) continue;
+					if(data[probe]->is_occupied() && data[probe]->key == key)
+						return probe;
+				}
+				return {};
+			}
+
+			std::optional<size_t> find_empty_spot(size_t start) const {
+				DOIR_ZONE_SCOPED_AGRO;
+				auto data = Base::data();
+				auto size = Base::size();
+				for(size_t i = 0; i < NeighborhoodSize; ++i) {
+					size_t probe = (start + i) % size;
+					if(!data[probe]->is_occupied())
+						return probe;
+				}
+				return {};
+			}
+
+			std::optional<size_t> find_nearest_neighbor(size_t start) const {
+				DOIR_ZONE_SCOPED_AGRO;
+				auto data = Base::data();
+				auto size = Base::size();
+				for(size_t j = 0; j < NeighborhoodSize; ++j) {
+					size_t probe = (start + j) % size;
+					if((data[start]->hopInfo & (1 << j)) && !data[probe]->is_occupied())
+						return probe;
+				}
+				return {};
+			}
+
+			inline bool is_in_neighborhood(size_t start, size_t needle) const {
+				DOIR_ZONE_SCOPED_AGRO;
+				if(NeighborhoodSize > Base::size()) return true;
+				size_t end = (start + NeighborhoodSize) % Base::size();
+				if(start <= end)
+					return needle >= start && needle <= end; // TODO: should be < instead?
+				else // The neighborhood range wraps around the end of the table
+					return needle >= start || needle <= end;
+			}
+
+			// bool insert_impl(module& module, const kv_pair& key_value, size_t retries = 0) {
+			// 	size_t hash = this->hash(key(key_value));
+
+			// 	auto emptyIndex = find_empty_spot(hash);
+			// 	if(!emptyIndex) {
+			// 		if(!resize_and_rehash(module)) return false;
+			// 		if(retries == 5) return false;
+			// 		return insert_impl(module, key_value, retries + 1);
+			// 	}
+
+			// 	if constexpr(has_value_type) {
+			// 		Base::data()[*emptyIndex]->key = key_value.first;
+			// 		Base::data()[*emptyIndex]->value = key_value.second;
+			// 	} else
+			// 		Base::data()[*emptyIndex].key = key_value;
+			// 	Base::data()[*emptyIndex]->set_occupied(true);
+			// 	if constexpr(store_hash) Base::data()[*emptyIndex].hash = hash;
+
+			// 	size_t distance = *emptyIndex > hash ? *emptyIndex - hash : Base::size() - (hash - *emptyIndex);
+			// 	Base::data()[hash]->hopInfo |= (1 << distance);
+			// 	return true;
+			// }
+
+			inline bool double_size_and_rehash(TrivialModule& module, size_t retries = 0) {
+				DOIR_ZONE_SCOPED_AGRO;
+				size_t size = Base::size();
+				Base::allocate(size);
+				auto data = Base::data();
+				for(size_t i = size, size = Base::size(); i < size; ++i)
+					data[i] = {}; // TODO: Nessicary?
+
+				// Make sure all of the new cells are initialized with invalid entities
+				for(size_t i = 0; i < size; ++i)
+					if(!data[i]->is_occupied())
+						data[i].entity = invalid_entity;
+				return rehash(module, retries, true);
+			}
+
+			bool rehash(TrivialModule& module, size_t retries, bool resized = false) {
+				DOIR_ZONE_SCOPED_AGRO;
+				// Clear the neighborhood information
+				auto data = Base::data();
+				size_t size = Base::size(), half = size / 2;
+				for(size_t i = 0; i < size; ++i) {
+					auto dbg = data[i];
+					bool occupied = data[i]->is_occupied();
+					data[i]->hopInfo = 0;
+					if(resized && occupied && i < half && (i % 2) == 1) { // Distribute every other element to the second half of the newly resized space
+						if(!Base::swap(module, i, size - i, true)) return false;
+						data[size - i]->set_occupied(occupied);
+						data[i]->set_occupied(false);
+					} else data[i]->set_occupied(occupied);
+				}
+
+				// Traverse through the old table and "reinsert" elements into the table
+				for(size_t i = 0; i < size; ++i)
+					if(data[i]->is_occupied()) {
+						size_t hash = this->hash(data[i]->key);
+
+						// If the value is already in the correct neighborhood... no need to move around just mark as present
+						if(is_in_neighborhood(hash, i)) {
+							if constexpr(store_hash) data[i]->hash = hash;
+							size_t distance = i > hash ? i - hash : size - (hash - i);
+							data[hash]->hopInfo |= (1 << distance);
+							continue;
+						}
+
+						// Find an empty spot in the new table starting from the new hash value
+						auto emptyIndex = find_empty_spot(hash);
+						if(!emptyIndex) {
+							if(retries >= MaxRetries) return false;
+							return double_size_and_rehash(module, retries + 1); // TODO: We can probably use a better strategy than "resize and try again"
+						}
+
+						// Move the element to its new position in the table
+						size_t hopInfoI = data[i]->hopInfo, hopInfoEmpty = data[*emptyIndex]->hopInfo;
+						if(!Base::swap(module, *emptyIndex, i, true)) return false;
+						data[i]->hopInfo = hopInfoI;
+						data[i]->set_occupied(false);
+						data[*emptyIndex]->hopInfo = hopInfoEmpty;
+						data[*emptyIndex]->set_occupied(true);
+
+						// Mark it as present in the element it hashes to
+						size_t distance = *emptyIndex > hash ? *emptyIndex - hash : size - (hash - *emptyIndex);
+						data[hash]->hopInfo |= (1 << distance);
+					}
+
+				return true;
+			}
+
+		public:
+			using component_type = component_t;
+
+			inline bool rehash(TrivialModule& module) {
+				DOIR_ZONE_SCOPED_AGRO;
+				return rehash(module, 0);
+			}
+
+			// bool insert(module& module, const kv_pair& key_value, bool maybeRehash = true) {
+			// 	if(maybeRehash && current_size() >= Base::size() * maxLoadFactor) // Takes a linear scan so that we don't need to store extra size information...
+			// 		resize_and_rehash(module);
+
+			// 	if(find_position(key(key_value)))
+			// 		return false;  // Key already exists
+
+			// 	if(!insert_impl(module, key_value)) return false;
+			// 	return true;
+			// }
+
+			inline std::optional<entity_t> find(const key_type& key) const {
+				DOIR_ZONE_SCOPED_AGRO;
+				if(auto index = find_position(key))
+					return Base::data()[*index].entity;
+				return {};  // Key not found
+			}
+
+			inline std::optional<entity_t> rehash_and_find(TrivialModule& module, const key_type& key) {
+				if(!rehash(module)) return {};
+				return find(key);
+			}
+
+			// bool remove(TrivialModule& module, const key_type& key) {
+			// 	auto index = find_position(key);
+			// 	if(!index)
+			// 		return false;  // Key not found
+
+			// 	// TODO: need to modify the module to mark that the entity no longer has a component
+
+			// 	Base::data()[*index].set_occupied(false);
+
+			// 	// Update hop information of neighbors
+			// 	auto hash = this->hash(key);
+			// 	for(size_t i = 0; i < NeighborhoodSize; ++i) {
+			// 		size_t probe = (hash + i) % Base::size();
+			// 		if(Base::data()[probe]->is_occupied())
+			// 			Base::data()[probe]->hopInfo &= ~(1 << (*index > i ? *index - i : Base::size() - (i - *index)));
+			// 	}
+
+			// 	return true;
+			// }
+		};
+
+		template<typename Tkey, typename Tvalue = void>
+		inline void mark_occupied(component_wrapper<Tkey, Tvalue>& comp) {
+			DOIR_ZONE_SCOPED_AGRO;
+			comp->set_occupied(true);
+		}
+		// template<typename Tkey, typename Tvalue = void>
+		// inline void mark_occupied(optional_reference<component_wrapper<Tkey, Tvalue>> opt) {
+		// 	opt.value()->set_occupied(true);
+		// }
+
+		template<typename Tkey, typename Tvalue = void>
+		inline Tkey& get_key(component_wrapper<Tkey, Tvalue>& comp) {
+			DOIR_ZONE_SCOPED_AGRO;
+			return comp->key;
+		}
+		// template<typename Tkey, typename Tvalue = void>
+		// inline Tkey& get_key(optional_reference<component_wrapper<Tkey, Tvalue>> opt) {
+		// 	return opt.value()->key;
+		// }
+
+		template<typename Tkey, typename Tvalue = void>
+		inline Tkey& get_key_and_mark_occupied(component_wrapper<Tkey, Tvalue>& comp) {
+			DOIR_ZONE_SCOPED_AGRO;
+			mark_occupied<Tkey, Tvalue>(comp);
+			return get_key<Tkey, Tvalue>(comp);
+		}
+		// template<typename Tkey, typename Tvalue = void>
+		// inline Tkey& get_key_and_mark_occupied(optional_reference<component_wrapper<Tkey, Tvalue>> opt) {
+		// 	mark_occupied<Tkey, Tvalue>(opt);
+		// 	return get_key<Tkey, Tvalue>(*opt);
+		// }
+
+		template<typename Tkey, typename Tvalue = void>
+			requires((!std::is_same_v<Tvalue, void>))
+		inline Tvalue& get_value(component_wrapper<Tkey, Tvalue>& comp) {
+			DOIR_ZONE_SCOPED_AGRO;
+			return comp.value.value;
+		}
+		// template<typename Tkey, typename Tvalue = void>
+		// 	requires((!std::is_same_v<Tvalue, void>))
+		// inline Tvalue& get_value(optional_reference<component_wrapper<Tkey, Tvalue>> comp) {
+		// 	return comp.value().value.value;
+		// }
+	}
+
+	namespace detail {
+		// Make sure machinery in module can detect that component wrapers are a type of with_entity
+		template<typename Tkey, typename Tvalue>
+		struct is_with_entity<ecs::hashtable::component_wrapper<Tkey, Tvalue>> : public std::true_type {};
+	}
+
+	using hashtable::get_key;
+	using hashtable::get_key_and_mark_occupied;
+	using hashtable::get_value;
+}
+
+#endif // __ECS_ADAPTER_HPP__
