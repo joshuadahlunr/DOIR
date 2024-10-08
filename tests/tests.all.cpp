a0314d4f807279be885d216b7e6c047be2d555f4
Joshua Dahl <joshuadahl@unr.edu>
2024-10-08 14:32:28 -0700

    ECS Basics
    Simple ECS implementation, profiled, benchmarked, and optimized using libfp.
    Both a C and C++ interface are provided.

---
+++ tests/tests.all.cpp
@@ -0,0 +1,25 @@
+#define ANKERL_NANOBENCH_IMPLEMENT
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "tests.utils.hpp"
+
+#include <cstdlib>
+
+#ifdef DOIR_MEMORY_PROFILING
+	inline void* profiled_alloc(void* p, size_t size) {
+		if(size == 0) {
+			TracyFreeNS(p, 10, "fp");
+			free(p);
+			return nullptr;
+		}
+
+		if(p) TracyFreeNS(p, 10, "fp");
+		p = realloc(p, size);
+		TracyAllocNS(p, size, 10, "fp");
+		return p;
+	}
+	#define FP_ALLOCATION_FUNCTION profiled_alloc
+#endif
+
+#define DOIR_IMPLEMENTATION
+#define FP_IMPLEMENTATION
+#include "../src/ECS/ecs.hpp"
