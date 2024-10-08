7d6c97be6d143674efd7ce8a2da66dcecab1a325
Joshua Dahl <joshuadahl@unr.edu>
2024-10-08 14:29:58 -0700

    [Windows] nowide
    Use https://github.com/nephatrine/nowide-standalone to simplify utf8 handling on windows.

--- tests/tests.utils.hpp
+++ tests/tests.utils.hpp
@@ -6,17 +6,17 @@
 #include "../src/utility/profile.config.hpp"
 
 #define CAPTURE_CONSOLE_BEGIN std::stringstream capture; \
-/* capture(std::cout) */{\
-	std::streambuf* std__cout_buf = std::cout.rdbuf();\
-	std::cout.rdbuf(capture.rdbuf());
+/* capture(nowide::cout) */{\
+	std::streambuf* std__cout_buf = nowide::cout.rdbuf();\
+	nowide::cout.rdbuf(capture.rdbuf());
 
-#define CAPTURE_CONSOLE_END std::cout.rdbuf(std__cout_buf); }
+#define CAPTURE_CONSOLE_END nowide::cout.rdbuf(std__cout_buf); }
 
 #define CAPTURE_ERROR_CONSOLE_BEGIN std::stringstream capture; \
-/* capture(std::cerr) */{\
-	std::streambuf* std__cerr_buf = std::cerr.rdbuf();\
-	std::cerr.rdbuf(capture.rdbuf());
+/* capture(nowide::cerr) */{\
+	std::streambuf* std__cerr_buf = nowide::cerr.rdbuf();\
+	nowide::cerr.rdbuf(capture.rdbuf());
 
-#define CAPTURE_ERROR_CONSOLE_END std::cerr.rdbuf(std__cerr_buf); }
+#define CAPTURE_ERROR_CONSOLE_END nowide::cerr.rdbuf(std__cerr_buf); }
 
-#include <iostream>
+#include <nowide/iostream.hpp>
