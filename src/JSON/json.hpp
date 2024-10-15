7ce5638a7d25c77629c0f29d062e891735664620
Joshua Dahl <joshuadahl@unr.edu>
2024-10-15 16:17:01 -0700

    [JSON] DOIR Port

--- src/JSON/json.hpp
+++ src/JSON/json.hpp
@@ -1,8 +1,9 @@
 #pragma once
 
-#include "../ECS/ecs.hpp"
+#include "../module.hpp"
 
 namespace doir::JSON {
-	char* parse_view(const fp_string_view view);
-	char* parse(const fp_string string);
+	std::pair<TrivialModule, ecs::entity_t> parse_view(const fp_string_view view);
+	std::pair<TrivialModule, ecs::entity_t> parse(const fp_string string);
+	fp_string dump(TrivialModule& module, ecs::entity_t root);
 }
