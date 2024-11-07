b1b834e7d88ea85e794f4fa58e8b975a884d1cf0
Joshua Dahl <joshuadahl@unr.edu>
2024-11-07 10:29:05 -0800

    [JSON] Glaze
    Rewrote tests using: https://github.com/stephenberry/glaze

--- tests/2_json.cpp
+++ tests/2_json.cpp
@@ -1,6 +1,7 @@
 #include "tests.utils.hpp"
 
 #include "../src/JSON/json.hpp"
+#include <glaze/glaze.hpp>
 
 TEST_SUITE("JSON") {
 
@@ -8,21 +9,22 @@ TEST_SUITE("JSON") {
 #ifdef DOIR_ENABLE_BENCHMARKING
 		ankerl::nanobench::Bench().run("doir::JSON::Atoms", []{
 #endif
+			glz::json_t json;
 			{DOIR_ZONE_SCOPED_NAMED("doir::JSON::Atoms::True");
-				auto [module, root] = doir::JSON::parse("true");
-				CHECK(fp_string_equal(doir::JSON::dump(module, root), "true"));
+				CHECK(!glz::read_json(json, "true"));
+				CHECK(glz::write_json(json).value() == "true");
 			}{DOIR_ZONE_SCOPED_NAMED("doir::JSON::Atoms::False");
-				auto [module, root] = doir::JSON::parse("false");
-				CHECK(fp_string_equal(doir::JSON::dump(module, root), "false"));
+				CHECK(!glz::read_json(json, "false"));
+				CHECK(glz::write_json(json).value() == "false");
 			}{DOIR_ZONE_SCOPED_NAMED("doir::JSON::Atoms::Null");
-				auto [module, root] = doir::JSON::parse("null");
-				CHECK(fp_string_equal(doir::JSON::dump(module, root), "null"));
+				CHECK(!glz::read_json(json, "null"));
+				CHECK(glz::write_json(json).value() == "null");
 			}{DOIR_ZONE_SCOPED_NAMED("doir::JSON::Atoms::Number");
-				auto [module, root] = doir::JSON::parse("5");
-				CHECK(fp_string_equal(doir::JSON::dump(module, root), "5.000000"));
+				CHECK(!glz::read_json(json, "5"));
+				CHECK(glz::write_json(json).value() == "5");
 			}{DOIR_ZONE_SCOPED_NAMED("doir::JSON::Atoms::String");
-				auto [module, root] = doir::JSON::parse("\"Hello World\"");
-				CHECK(fp_string_equal(doir::JSON::dump(module, root), "\"Hello World\""));
+				CHECK(!glz::read_json(json, "\"Hello World\""));
+				CHECK(glz::write_json(json).value() == "\"Hello World\"");
 			}
 #ifdef DOIR_ENABLE_BENCHMARKING
 		});
@@ -35,8 +37,9 @@ TEST_SUITE("JSON") {
 		ankerl::nanobench::Bench().run("doir::JSON::Array", []{
 #endif
 			DOIR_ZONE_SCOPED_NAMED("doir::JSON::Array");
-			auto [module, root] = doir::JSON::parse("[true, false, null, 5.5, \"Hello\"]");
-			CHECK(fp_string_equal(doir::JSON::dump(module, root), "[true,false,null,5.500000,\"Hello\"]"));
+			glz::json_t json;
+			CHECK(!glz::read_json(json, "[true, false, null, 5.5, \"Hello\"]"));
+			CHECK(glz::write_json(json).value() == "[true,false,null,5.5,\"Hello\"]");
 
 #ifdef DOIR_ENABLE_BENCHMARKING
 		});
@@ -49,8 +52,11 @@ TEST_SUITE("JSON") {
 		ankerl::nanobench::Bench().run("doir::JSON::Object", []{
 #endif
 			DOIR_ZONE_SCOPED_NAMED("doir::JSON::Object");
-			auto [module, root] = doir::JSON::parse("{\"bool\": true, \"!bool\": false, \"null\": null, \"num\": 5.5, \"str\": \"Hello\"}");
-			CHECK(fp_string_equal(doir::JSON::dump(module, root), "{\"bool\":true,\"!bool\":false,\"null\":null,\"num\":5.500000,\"str\":\"Hello\"}"));
+			glz::json_t json;
+			CHECK(!glz::read_json(json, "{\"bool\": true, \"!bool\": false, \"null\": null, \"num\": 5.5, \"str\": \"Hello\"}"));
+			CHECK(glz::write_json(json).value() == "{\"!bool\":false,\"bool\":true,\"null\":null,\"num\":5.5,\"str\":\"Hello\"}");
+			// auto [module, root] = doir::JSON::parse("{\"bool\": true, \"!bool\": false, \"null\": null, \"num\": 5.5, \"str\": \"Hello\"}");
+			// CHECK(fp_string_equal(doir::JSON::dump(module, root), "{\"bool\":true,\"!bool\":false,\"null\":null,\"num\":5.500000,\"str\":\"Hello\"}"));
 
 #ifdef DOIR_ENABLE_BENCHMARKING
 		});
@@ -64,9 +70,9 @@ TEST_SUITE("JSON") {
 #endif
 			DOIR_ZONE_SCOPED_NAMED("doir::JSON::stephenberry");
 			// Data from: https://github.com/stephenberry/json_performance
-			auto [module, root] = doir::JSON::parse(R"({"fixed_object":{"int_array":[0,1,2,3,4,5,6],"float_array":[0.1,0.2,0.3,0.4,0.5,0.6],"double_array":[3288398.238,2.33e+24,28.9,0.928759872,0.22222848,0.1,0.2,0.3,0.4]},"fixed_name_object":{"name0":"James","name1":"Abraham","name2":"Susan","name3":"Frank","name4":"Alicia"},"another_object":{"string":"here is some text","another_string":"Hello World","escaped_text":"{\"some key\":\"some string value\"}","boolean":false,"nested_object":{"v3s":[[0.12345,0.23456,0.001345],[0.3894675,97.39827,297.92387],[18.18,87.289,2988.298]],"id":"298728949872"}},"string_array":["Cat","Dog","Elephant","Tiger"],"string":"Hello world","number":3.14,"boolean":true,"another_bool":false})");
-			auto eq = fp_string_equal(doir::JSON::dump(module, root), R"({"fixed_object":{"int_array":[0.000000,1.000000,2.000000,3.000000,4.000000,5.000000,6.000000],"float_array":[0.100000,0.200000,0.300000,0.400000,0.500000,0.600000],"double_array":[3288398.238000,2330000000000000062914560.000000,28.900000,0.928760,0.222228,0.100000,0.200000,0.300000,0.400000]},"fixed_name_object":{"name0":"James","name1":"Abraham","name2":"Susan","name3":"Frank","name4":"Alicia"},"another_object":{"string":"here is some text","another_string":"Hello World","escaped_text":"{\"some key\":\"some string value\"}","boolean":false,"nested_object":{"v3s":[[0.123450,0.234560,0.001345],[0.389468,97.398270,297.923870],[18.180000,87.289000,2988.298000]],"id":"298728949872"}},"string_array":["Cat","Dog","Elephant","Tiger"],"string":"Hello world","number":3.140000,"boolean":true,"another_bool":false})");
-			CHECK(eq);
+			glz::json_t json;
+			CHECK(!glz::read_json(json, R"({"fixed_object":{"int_array":[0,1,2,3,4,5,6],"float_array":[0.1,0.2,0.3,0.4,0.5,0.6],"double_array":[3288398.238,2.33e+24,28.9,0.928759872,0.22222848,0.1,0.2,0.3,0.4]},"fixed_name_object":{"name0":"James","name1":"Abraham","name2":"Susan","name3":"Frank","name4":"Alicia"},"another_object":{"string":"here is some text","another_string":"Hello World","escaped_text":"{\"some key\":\"some string value\"}","boolean":false,"nested_object":{"v3s":[[0.12345,0.23456,0.001345],[0.3894675,97.39827,297.92387],[18.18,87.289,2988.298]],"id":"298728949872"}},"string_array":["Cat","Dog","Elephant","Tiger"],"string":"Hello world","number":3.14,"boolean":true,"another_bool":false})"));
+			CHECK(glz::write_json(json).value() == R"({"another_bool":false,"another_object":{"another_string":"Hello World","boolean":false,"escaped_text":"{\"some key\":\"some string value\"}","nested_object":{"id":"298728949872","v3s":[[0.12345,0.23456,0.001345],[0.3894675,97.39827,297.92387],[18.18,87.289,2988.298]]},"string":"here is some text"},"boolean":true,"fixed_name_object":{"name0":"James","name1":"Abraham","name2":"Susan","name3":"Frank","name4":"Alicia"},"fixed_object":{"double_array":[3288398.238,2.33E24,28.9,0.928759872,0.22222848,0.1,0.2,0.3,0.4],"float_array":[0.1,0.2,0.3,0.4,0.5,0.6],"int_array":[0,1,2,3,4,5,6]},"number":3.14,"string":"Hello world","string_array":["Cat","Dog","Elephant","Tiger"]})");
 
 #ifdef DOIR_ENABLE_BENCHMARKING
 		});
